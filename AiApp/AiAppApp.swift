2781b4f9e4709d9c8a1fec26bcddba9624982c27
Salvatore Milazzo <salvatore.milazzo@objectway.com>
2023-05-25 14:44:03 +0200

    Initial Commit

---
+++ AiApp/AiAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AiAppApp.swift
+//  AiApp
+//
+//  Created by Salvatore Milazzo on 25/05/23.
+//
+
+import SwiftUI
+
+@main
+struct AiAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
