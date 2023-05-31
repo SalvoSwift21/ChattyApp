9298a5aa91e805f0262ea0bb2758ddfdc01e02e7
Salvatore Milazzo <salvo.milazzo.21@gmail.com>
2023-05-31 20:52:01 +0200

    Create first version of httpclient, convert normal get request to combine request

---
+++ RestApi/RestApi/API Infra/URLSessionHTTPClient.swift
@@ -0,0 +1,53 @@
+//
+//  Copyright © Essential Developer. All rights reserved.
+//
+
+import Foundation
+import Combine
+
+public final class URLSessionHTTPClient: HTTPClient {
+    
+	private let session: URLSession
+	
+	public init(session: URLSession) {
+		self.session = session
+	}
+	
+	private struct UnexpectedValuesRepresentation: Error {}
+	
+	private struct URLSessionTaskWrapper: HTTPClientTask {
+		let wrapped: URLSessionTask
+		
+		func cancel() {
+			wrapped.cancel()
+		}
+	}
+    
+    public func makeRequest(from url: URLRequest) -> (HTTPClient.Publisher) {
+        return session.dataTaskPublisher(for: url)
+            .tryMap { data, response -> (Data, HTTPURLResponse) in
+                guard let httpResponse = response as? HTTPURLResponse else {
+                    throw UnexpectedValuesRepresentation()
+                }
+                return (data, httpResponse)
+            }
+            .mapError { $0 as Error }
+            .eraseToAnyPublisher()
+    }
+    
+	public func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
+		let task = session.dataTask(with: url) { data, response, error in
+			completion(Result {
+				if let error = error {
+					throw error
+				} else if let data = data, let response = response as? HTTPURLResponse {
+					return (data, response)
+				} else {
+					throw UnexpectedValuesRepresentation()
+				}
+			})
+		}
+		task.resume()
+		return URLSessionTaskWrapper(wrapped: task)
+	}
+}
