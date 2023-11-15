cd51477318e371b9212635520155642f09ef33f3
Salvatore Milazzo <salvo.milazzo.21@gmail.com>
2023-11-15 08:27:45 +0100

    Create new project

---
+++ ScanUI/ScanUI/ScanUI.h
@@ -0,0 +1,18 @@
+//
+//  ScanUI.h
+//  ScanUI
+//
+//  Created by Salvatore Milazzo on 15/11/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ScanUI.
+FOUNDATION_EXPORT double ScanUIVersionNumber;
+
+//! Project version string for ScanUI.
+FOUNDATION_EXPORT const unsigned char ScanUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ScanUI/PublicHeader.h>
+
+
